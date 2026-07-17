098a970e689ab313939f5f9759975877eb0eb18e
MHarrisT <harristabassum151@gmail.com>
2026-07-17 17:10:24 +0500

    Code updated till Lecture 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import MyFunc from "./functions"
 
 function App() {
+  const username = "harris"
   return (
     <>
       <MyFunc></MyFunc>
-      <h3>Hello World</h3>
+      <h3>Hello {username}</h3>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react' // <-- Added 'React' here
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+
+// Lecture 4 code:
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click this to open google'
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+  // reactElement
 )
